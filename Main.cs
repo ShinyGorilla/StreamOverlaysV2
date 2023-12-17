48576bb194d2eea9af4ab12f23ddabbda9af4b5e
ShinyGorilla <134111158+ShinyGorilla@users.noreply.github.com>
2023-12-16 17:31:18 -0700

    Update Main.cs

--- Main.cs
+++ Main.cs
@@ -1,57 +1,63 @@
-﻿using System;
+using System;
 using BepInEx;
+using Pathfinding.RVO.Sampled;
 using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 
 namespace StreamOverlaysV2
 {
-    [BepInPlugin("ShinyGorilla.StreamOverlaysV2", "StreamOverlaysV2", "1.0.0")]
+    [BepInPlugin("ShinyGorilla.StreamOverlaysV2", "StreamOverlaysV2", "1.0.1")]
     public class Main : BaseUnityPlugin
     {
+        public static Font Agency = Font.CreateDynamicFontFromOSFont("Agency FB", 24);
+        public static bool Enabled;
         private void OnEnable()
         {
             HarmonyPatches.ApplyHarmonyPatches();
+            Enabled = true;
         }
 
         private void OnDisable()
         {
             HarmonyPatches.RemoveHarmonyPatches();
+            Enabled = false;
         }
 
         private void OnGUI()
         {
-            GUI.skin.toggle.fontSize = 7;
-            GUI.skin.label.font = Main.agency;
-            GUI.skin.toggle.font = Main.agency;
-            GUI.skin.label.fontStyle = FontStyle.Italic;
-            GUI.skin.toggle.fontStyle = FontStyle.Italic;
-            GUI.color = new Color32(255, 255, 255, byte.MaxValue);
-
-            if (PhotonNetwork.InRoom)
+            if (Enabled)
             {
-                GUILayout.BeginVertical(Array.Empty<GUILayoutOption>());
+                GUI.skin.toggle.fontSize = 7;
+                GUI.skin.label.font = Agency;
+                GUI.skin.toggle.font = Agency;
+                GUI.skin.label.fontStyle = FontStyle.Normal;
+                GUI.skin.toggle.fontStyle = FontStyle.Normal;
+                GUI.color = new Color32(255, 255, 255, byte.MaxValue);
 
-                GUILayout.Label(string.Concat(new string[]
+                if (PhotonNetwork.InRoom)
                 {
+                    GUILayout.BeginVertical(Array.Empty<GUILayoutOption>());
+
+                    GUILayout.Label(string.Concat(new string[]
+                    {
                     "Code:",
                     PhotonNetwork.CurrentRoom.Name,
                     "\n",
                     PhotonNetwork.CurrentRoom.PlayerCount.ToString(),
                     "/10 players"
-                }), Array.Empty<GUILayoutOption>());
-                GUILayout.EndHorizontal();
-            }
+                    }), Array.Empty<GUILayoutOption>());
+                    GUILayout.EndHorizontal();
+                }
 
-            GUILayout.Space(5f);
+                GUILayout.Space(5f);
 
-            if (!PhotonNetwork.InRoom)
-            {
-                GUILayout.Label("Not connected to room", Array.Empty<GUILayoutOption>());
-                return;
+                if (!PhotonNetwork.InRoom)
+                {
+                    GUILayout.Label("Not connected to room", Array.Empty<GUILayoutOption>());
+                    return;
+                }
             }
         }
-
-        public static Font agency = Font.CreateDynamicFontFromOSFont("Agency FB", 24);
     }
 }
